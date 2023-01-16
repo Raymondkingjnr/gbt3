94f64677fbbe44eabc7caecbc527daa192537443
Raymondkingjnr <raymondnnaji660@gmail.com>
2023-01-16 22:33:40 +0100

    renamed:    src/assets/Rectangle 22.png -> src/assets/blog1.png 	renamed:    src/assets/Rectangle 22 (1).png -> src/assets/blog2.png 	renamed:    src/assets/Rectangle 22 (4).png -> src/assets/blog3.png 	renamed:    src/assets/Rectangle 22 (3).png -> src/assets/blog4.png 	renamed:    src/assets/Rectangle 22 (2).png -> src/assets/blog5.png 	modified:   src/componenets/article/Article.jsx 	modified:   src/componenets/article/article.css 	modified:   src/componenets/cta/Cta.jsx 	modified:   src/componenets/cta/cta.css 	modified:   src/componenets/feature/Feature.jsx 	modified:   src/containers/blog/Blog.jsx 	modified:   src/containers/blog/blog.css 	new file:   src/containers/blog/imports.js 	modified:   src/containers/features/Features.jsx 	modified:   src/containers/features/features.css 	modified:   src/containers/possibility/Possibility.jsx 	modified:   src/containers/possibility/possibility.css 	modified:   src/containers/whatGBT3/WhatGBT3.jsx 	modified:   src/containers/whatGBT3/whatGBT3.css

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Article } from "../../componenets";
 import "./blog.css";
+import { blog1, blog2, blog3, blog4, blog5 } from "./imports";
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgurl={blog1}
+            date="sep 26, 2011"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            imgurl={blog2}
+            date="sep 26, 2011"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgurl={blog3}
+            date="sep 26, 2011"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgurl={blog4}
+            date="sep 26, 2011"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgurl={blog5}
+            date="sep 26, 2011"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Blog;
